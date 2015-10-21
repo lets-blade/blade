0b364fb8e039182e34f3eb4fc119dd9f70868b0a
biezhi <biezhi.me@gmail.com>
2015-10-21 13:34:42 +0800

    update blade VERSION

--- blade-core/src/main/java/com/blade/Blade.java
+++ blade-core/src/main/java/com/blade/Blade.java
@@ -44,7 +44,7 @@ import blade.kit.json.JSONKit;
  */
 public class Blade {
 	
-	public static final String VERSION = "1.4.0-alpha";
+	public static final String VERSION = "1.4.0-beta";
 	
 	private static final Blade ME = new Blade();
 	
