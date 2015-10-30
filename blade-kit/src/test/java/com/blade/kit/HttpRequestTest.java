e985696be851e4dab4bd825a6c8957e0ea126033
biezhi <biezhi.me@gmail.com>
2015-10-30 11:29:34 +0800

    add blade-kit test code

---
+++ blade-kit/src/test/java/com/blade/kit/HttpRequestTest.java
@@ -0,0 +1,33 @@
+package com.blade.kit;
+
+import java.io.File;
+
+import blade.kit.http.HttpRequest;
+
+public class HttpRequestTest {
+	
+	public static void main(String[] args) {
+		
+		// 发送一个GET请求并获取内容
+		String body = HttpRequest.get("http://bladejava.com").body();
+		System.out.println(body);
+		
+		// 发送一个带heder的请求
+		String res = HttpRequest.get("http://bladejava.com").accept("application/json")
+				.userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0").body();
+		System.out.println(res);
+		
+		// 发送一个POST请求
+		HttpRequest.post("http://www.example.com").part("id", 20).part("name", "jack").body();
+		
+		// 带认证的请求
+		int response = HttpRequest.get("http://google.com").basic("username", "p4ssw0rd").code();
+		System.out.println(response);
+		
+		// 下载一个图片
+		File file = new File("F:/a.png");
+		HttpRequest.get("http://img.blog.csdn.net/20150601232126808").receive(file);
+		
+	}
+	
+}
---
+++ blade-kit/src/test/java/com/blade/kit/TaskTest.java
@@ -0,0 +1,35 @@
+package com.blade.kit;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import blade.kit.TaskKit;
+
+
+public class TaskTest {
+
+	public static void main(String[] args) {
+		TaskKit.scheduleAtFixedRate(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("每隔两秒执行一次");
+			}
+		}, 2);
+		
+		TaskKit.scheduleAtFixedRate(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("在指定的延时之后开始以固定的频率来运行任务。后续任务的启动时间不受前次任务延时影响。");
+			}
+		}, 10, 3, TimeUnit.SECONDS);
+		
+		TaskKit.scheduleAtFixedRate(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("在指定的时间点启动，两次任务间保持固定的时间间隔");
+			}
+		}, new Date(), 3, TimeUnit.SECONDS);
+		
+	}
+	
+}
