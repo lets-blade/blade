1952c64648c7c3bfed602acc72a823dad52cd98c
biezhi <biezhi.me@gmail.com>
2016-02-17 18:13:58 +0800

    update log

--- blade-redis/src/main/java/blade/plugin/redis/RedisPlugin.java
+++ blade-redis/src/main/java/blade/plugin/redis/RedisPlugin.java
@@ -3,11 +3,10 @@ package blade.plugin.redis;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.blade.plugin.Plugin;
 
+import blade.kit.logging.Logger;
+import blade.kit.logging.LoggerFactory;
 import redis.clients.jedis.JedisShardInfo;
 
 /**
