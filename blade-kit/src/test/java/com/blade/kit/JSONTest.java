89bbe6929fa2bd01009ca0fec3257163fe3f6bb6
gufengwyx8 <466144425@qq.com>
2016-04-26 00:32:39 +0800

    add unit test for JSONTest

--- blade-kit/src/test/java/com/blade/kit/JSONTest.java
+++ blade-kit/src/test/java/com/blade/kit/JSONTest.java
@@ -11,49 +11,51 @@ import blade.kit.json.JSONHelper;
 import blade.kit.json.JSONKit;
 import blade.kit.json.JSONObject;
 import blade.kit.json.JSONValue;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class JSONTest {
 
-	public static void main(String[] args) {
-		//[{"text": "首页","href": "/"},{"text": "博客","href": "blog"},{"text": "关于","href": "about"},{"text": "联系","href": "contact"}]
-		String json = "[{\"text\": \"博客\",\"href\": \"blog\"}]";
-		
-		// 下面构造两个map、一个list和一个Employee对象
-        Map<String, String> map1 = new HashMap<String, String>();
-        map1.put("name", "Alexia");
-        map1.put("sex", "female");
-        map1.put("age", "23");
+    @Test
+    public void testModifyJsonObject() {
+        JSONObject obj1 = new JSONObject();
+        obj1.put("name", "jack");
+        Assert.assertEquals(obj1.getString("name"), "jack");
 
+        JSONArray obj2 = new JSONArray();
+        obj2.add("123");
+
+        Assert.assertEquals(obj2.toString(), "[\"123\"]");
+
+        User u1 = new User();
+        u1.setAge(22);
+        u1.setName("rose");
+
+        Assert.assertEquals(JSONHelper.toJSONValue(u1).toString(), "{\"name\":\"rose\",\"age\":22}");
+    }
+
+    @Test
+    public void testParseJsonArray() {
+        String json = "[{\"abc\":\"123456\",\"def\":\"hmm\"}]";
+        List<JSONValue> list2 = JSON.parse(json).asArray().values();
+        Assert.assertFalse(list2.isEmpty());
+
+        JSONValue value = list2.get(0);
+        JSONObject obj = value.asJSONObject();
+        Assert.assertEquals(obj.getString("abc"), "123456");
+        Assert.assertEquals(obj.getString("def"), "hmm");
+    }
+
+    @Test
+    public void testToJSONString() {
         List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
         Map<String, Object> map = new HashMap<String, Object>();
 
         map.put("abc", "123456");
         map.put("def", "hmm");
-        list.add(map);	
-        
+        list.add(map);
+
         String string = JSONKit.toJSONString(list);
-        System.out.println(string);
-        
-		List<JSONValue> list2 = JSON.parse(json).asArray().values();
-		System.out.println(list2);
-        
-        JSONObject obj1 = new JSONObject();
-        obj1.put("name", "jack");
-        
-        System.out.println(obj1);
-        
-        JSONArray obj2 = new JSONArray();
-        obj2.add("123");
-        
-        System.out.println(obj2);
-        
-        User u1 = new User();
-        u1.setAge(22);
-//        u1.setName("rose");
-        
-        System.out.println(JSONHelper.toJSONValue(u1).toString());
-        
-//        System.out.println(JSONKit.toJSONString(u1));
-	}
-	
+        Assert.assertEquals(string, "[{\"abc\":\"123456\",\"def\":\"hmm\"}]");
+    }
 }
