2243ab57ec9021905441a6f157e431d30a39c048
biezhi <biezhi.me@gmail.com>
2017-07-10 22:35:30 +0800

    🥗 add some ason method

--- src/main/java/com/blade/kit/json/Ason.java
+++ src/main/java/com/blade/kit/json/Ason.java
@@ -1,8 +1,12 @@
 package com.blade.kit.json;
 
+import com.blade.kit.StringKit;
+
 import java.util.LinkedHashMap;
 
 /**
+ * Ason
+ *
  * @author biezhi
  *         2017/6/6
  */
@@ -17,6 +21,54 @@ public class Ason<K, V> extends LinkedHashMap<K, V> {
 
     public String getString(String key) {
         return this.get(key).toString();
+    }
+
+    public Integer getInt(String key) {
+        String val = getString(key);
+        if (StringKit.isNotBlank(val)) {
+            return Integer.valueOf(val);
+        }
+        return null;
+    }
+
+    public Long getLong(String key) {
+        String val = getString(key);
+        if (StringKit.isNotBlank(val)) {
+            return Long.valueOf(val);
+        }
+        return null;
+    }
+
+    public Boolean getBoolean(String key) {
+        String val = getString(key);
+        if (StringKit.isNotBlank(val)) {
+            return Boolean.valueOf(val);
+        }
+        return null;
+    }
+
+    public Double getDouble(String key) {
+        String val = getString(key);
+        if (StringKit.isNotBlank(val)) {
+            return Double.valueOf(val);
+        }
+        return null;
+    }
+
+    public Float getFloat(String key) {
+        String val = getString(key);
+        if (StringKit.isNotBlank(val)) {
+            return Float.valueOf(val);
+        }
+        return null;
+    }
+
+    public Short getShort(String key) {
+        String val = getString(key);
+        if (StringKit.isNotBlank(val)) {
+            return Short.valueOf(val);
+        }
+        return null;
     }
 
     @Override
