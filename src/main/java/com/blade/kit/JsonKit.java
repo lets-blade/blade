cffa1aa542ae5c0e320992e0c4547d59ea8084e4
biezhi <biezhi.me@gmail.com>
2017-09-20 15:13:49 +0800

    🐀 daily optimize

--- src/main/java/com/blade/kit/JsonKit.java
+++ src/main/java/com/blade/kit/JsonKit.java
@@ -5,6 +5,7 @@ import com.blade.kit.json.BeanSerializer;
 import com.blade.kit.json.JsonSerializer;
 import com.blade.kit.json.SerializeMapping;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Json kit
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
  * @author biezhi
  * 2017/6/2
  */
+@Slf4j
 @NoArgsConstructor
 public final class JsonKit {
 
@@ -20,8 +22,13 @@ public final class JsonKit {
     }
 
     public static String toString(Object object, SerializeMapping serializeMapping) {
-        Object jsonObj = BeanSerializer.serialize(serializeMapping, object);
-        return JsonSerializer.serialize(jsonObj);
+        try {
+            Object jsonObj = BeanSerializer.serialize(serializeMapping, object);
+            return JsonSerializer.serialize(jsonObj);
+        } catch (Exception e) {
+            log.error("object to json string error", e);
+            return null;
+        }
     }
 
     public static Ason toAson(String json) {
