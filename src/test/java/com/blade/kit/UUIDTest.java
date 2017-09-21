aca1b4ebc0f3023754402157aea1d158cfa74422
biezhi <biezhi.me@gmail.com>
2017-09-21 21:25:48 +0800

    🐀 daily optimize

--- src/test/java/com/blade/kit/UUIDTest.java
+++ src/test/java/com/blade/kit/UUIDTest.java
@@ -1,15 +1,8 @@
 package com.blade.kit;
 
-import org.junit.Test;
-
 /**
  * @author biezhi
  * @date 2017/9/21
  */
 public class UUIDTest {
-
-    @Test
-    public void testUUID(){
-        System.out.println(UUID.UU("abcd1234efgh5678"));
-    }
 }
