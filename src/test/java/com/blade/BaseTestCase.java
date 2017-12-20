9165a37c142c829e67b2656b1e32a3075a9c4869
biezhi <biezhi.me@gmail.com>
2017-12-21 00:01:39 +0800

    :penguin: temporarily turn off this test

--- src/test/java/com/blade/BaseTestCase.java
+++ src/test/java/com/blade/BaseTestCase.java
@@ -19,7 +19,7 @@ public class BaseTestCase {
     protected Blade app;
     private   String origin    = "http://127.0.0.1:10086";
     protected String firefoxUA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 Firefox/53.0";
-    protected volatile boolean isStarted;
+    private static volatile boolean isStarted;
 
     @Before
     public void setup() throws Exception {
