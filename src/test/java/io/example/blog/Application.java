abcfacdbcf0e5cb9ee749cb77622b8f7ecfcc874
biezhi <biezhi.me@gmail.com>
2017-12-20 23:01:51 +0800

    :penguin: temporarily turn off this test

--- src/test/java/io/example/blog/Application.java
+++ src/test/java/io/example/blog/Application.java
@@ -1,8 +1,5 @@
 package io.example.blog;
 
-import com.blade.Blade;
-import com.blade.security.web.csrf.CsrfMiddleware;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,16 +18,16 @@ public class Application {
         map.put("name", "blade");
         map.put("jdk", 1.8);
 
-        Blade.me()
+//        Blade.me()
 //                .devMode(false)
-                .use((invoker) -> {
-                    System.out.println("hello...");
-                    return true;
-                }, new CsrfMiddleware())
-                .get("/json", ((request, response) -> response.json(map)))
-                .showFileList(true)
-                .listen(9001)
-                .start(Application.class, args);
+//                .use((invoker) -> {
+//                    System.out.println("hello...");
+//                    return true;
+//                }, new CsrfMiddleware())
+//                .get("/json", ((request, response) -> response.json(map)))
+//                .showFileList(true)
+//                .listen(9001)
+//                .start(Application.class, args);
     }
 
 }
