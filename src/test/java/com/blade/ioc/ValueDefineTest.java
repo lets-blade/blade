424f27f53f47b7ecec7f9145d05fc05fa9de19d5
biezhi <biezhi.me@gmail.com>
2017-12-26 16:30:35 +0800

    :white_check_mark: fix test code

--- src/test/java/com/blade/ioc/ValueDefineTest.java
+++ src/test/java/com/blade/ioc/ValueDefineTest.java
@@ -1,6 +1,7 @@
 package com.blade.ioc;
 
 import com.blade.Blade;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,22 +11,33 @@ import org.junit.Test;
  * Date: 2017/12/25
  */
 public class ValueDefineTest {
-    Blade app = Blade.me();
+
+    private Blade app;
+
     @Before
     public void before() {
+        app = Blade.me();
         app.scanPackages("com.blade.model","com.blade.ioc");
-        app.listen(10086).start().await();
+        app.listen(10087).start().await();
     }
+
+    @After
+    public void after() {
+        app.stop();
+    }
+
     @Test
     public void testValueFromClass() {
         Assert.assertNotNull(app.ioc().getBean("com.blade.model.AppInfo"));
         System.out.println(app.ioc().getBean("com.blade.model.AppInfo"));
         //echo AppInfo{users='301', maxMoney='38.1', sex='true', hits='199283818033', startDate='2017-08-02'}
     }
+
     @Test
     public void testValueFromPro() {
         Assert.assertNotNull(app.ioc().getBean("com.blade.model.ValueBean"));
         System.out.println(app.ioc().getBean("com.blade.model.ValueBean"));
         //echo ValueBean{list=[hello, my, blade, word], appversion='0.0.2', map={user=blade, version=2.0.0}}
     }
+
 }
