6dab407f03ed94fd76edf805b35b7a4e2b50f13f
biezhi <biezhi.me@gmail.com>
2017-12-26 16:32:52 +0800

    :white_check_mark: fix test code

--- src/test/java/com/blade/model/ValueBean.java
+++ src/test/java/com/blade/model/ValueBean.java
@@ -2,6 +2,7 @@ package com.blade.model;
 
 import com.blade.ioc.annotation.Bean;
 import com.blade.ioc.annotation.Value;
+import lombok.Data;
 
 import java.util.List;
 import java.util.Map;
@@ -11,20 +12,16 @@ import java.util.Map;
  * Date: 2017/12/25
  */
 @Bean
+@Data
 public class ValueBean {
+
     @Value(name = "list")
     private List<String> list;
+
     @Value(name = "app.version")
     private String appversion;
+
     @Value(name = "map")
     private Map<String,String> map;
 
-    @Override
-    public String toString() {
-        return "ValueBean{" +
-                "list=" + list +
-                ", appversion='" + appversion + '\'' +
-                ", map=" + map +
-                '}';
-    }
 }
