78b299364e7f958fabaf6e08c863efc6a08f4d02
biezhi <biezhi.me@gmail.com>
2018-06-02 13:10:52 +0800

    :bug: fixed test compile error

--- src/test/java/com/blade/model/Person.java
+++ src/test/java/com/blade/model/Person.java
@@ -1,15 +1,16 @@
 package com.blade.model;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 public class Person {
 
     private String name;
     private String text;
     private int age;
+
+    public Person() {
+    }
 
     public Person(String name, String text, int age) {
         this.name = name;
