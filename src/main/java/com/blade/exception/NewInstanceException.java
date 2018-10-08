93b8e77e0e8804539d90a50b17b7e4bcf81f66e4
crossoverJie <crossoverjie@gmail.com>
2018-10-08 23:28:06 +0800

    :busts_in_silhouette: Add contributor(s).

--- src/main/java/com/blade/exception/NewInstanceException.java
+++ src/main/java/com/blade/exception/NewInstanceException.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018, crossoverJie (crossoverJie.me@gmail.com)
+ * Copyright (c) 2018, crossoverJie (crossoverJie@gmail.com)
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
