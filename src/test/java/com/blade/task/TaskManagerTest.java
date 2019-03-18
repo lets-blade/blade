e937ee4a57e8aafaaf566d7dd2e624b3a963edea
psh686868 <psh686868@163.com>
2019-03-18 13:30:55 +0800

    :update: update TackManager

--- src/test/java/com/blade/task/TaskManagerTest.java
+++ src/test/java/com/blade/task/TaskManagerTest.java
@@ -2,26 +2,29 @@ package com.blade.task;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.stream.IntStream;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * @author PSH
  * @date 2019/03/16
  */
 public class TaskManagerTest {
-    public static void main(String[] args) {
-        CountDownLatch downLatch = new CountDownLatch(500);
-        IntStream.range(0,500).forEach(i -> {
+
+    @Test
+    public void testAddTaskMultiThreading() throws Exception {
+
+        final int tackCount = 500;
+        CountDownLatch downLatch = new CountDownLatch(tackCount);
+        IntStream.range(0, tackCount).forEach(i -> {
             Task task = new Task("task-" + i, null, Integer.MAX_VALUE);
-            new Thread(()->{
+            new Thread(() -> {
                 TaskManager.addTask(task);
                 downLatch.countDown();
             }).start();
         });
-        try {
-            downLatch.await();
-            System.out.println("TaskManager.getTasks().size() = " + TaskManager.getTasks().size());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+        downLatch.await();
+        Assert.assertEquals(tackCount, TaskManager.getTasks().size());
     }
 }
