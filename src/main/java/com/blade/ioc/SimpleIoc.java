a0d20479d59fac9c9561676293d50c68c94c679a
NetoDevel <josevieira.dev@gmail.com>
2020-04-21 14:43:22 -0300

    Fixs build

--- src/main/java/com/blade/ioc/SimpleIoc.java
+++ src/main/java/com/blade/ioc/SimpleIoc.java
@@ -7,6 +7,7 @@ import com.blade.mvc.WebContext;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * The default IOC container implementation
